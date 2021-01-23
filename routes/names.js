2923e4eeaea2b1fd8b7741f647e6a2599c4a2178
nordluma <nordluma@arcada.fi>
2021-01-23 12:52:03 +0200

    fixed export

--- routes/names.js
+++ routes/names.js
@@ -1,6 +1,6 @@
 const router = require("express").Router();
 
-const getAllNames = require("../controllers/names");
+const { getAllNames } = require("../controllers/names");
 
 router.get("/", getAllNames);
 
